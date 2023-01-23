36514ad1f720b14bf937ec88fc5881441b3c9db1
Ralph Gradison <rgradison@gmail.com>
2023-01-23 10:07:29 +0200

    Added IdNumberValidator class

--- src/test/java/com/example/clientsys/utils/ClientControllerValidatorTest.java
+++
@@ -1,4 +0,0 @@
-package com.example.clientsys.utils;
-
-public class ClientControllerValidatorTest {
-}
---
+++ src/test/java/com/example/clientsys/utils/IdNumberValidatorTest.java
@@ -0,0 +1,19 @@
+package com.example.clientsys.utils;
+
+import com.utils.IdNumberValidator;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class IdNumberValidatorTest {
+    private IdNumberValidator underTest;
+    @BeforeEach
+    void setUp() throws Exception {
+        underTest = new IdNumberValidator();
+    }
+
+    @Test
+    void itShouldValidateIdNumber() throws Exception {
+
+    }
+
+}
